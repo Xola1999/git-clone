e84ed1eb595dbc2099293450352f728b6a0c03d0
Xola1999 <axolalose@icloud.com>
2024-01-10 23:01:05 +0200

    fix changes # Refactor the loop for better readability

--- issuie1.js
+++ issuie1.js
@@ -1,8 +1,17 @@
+/**
+ * Improved version of the printGroceryList function.
+ * @param {string[]} groceryItems - The list of grocery items to be printed.
+ */
 function printGroceryList(groceryItems) {
     console.log("Grocery List:");
-    for (let i = 0; i < groceryItems.length; i++) {
-        console.log("*" + groceryItems[i]);
+
+    // Refactor the loop for better readability
+    for (let item of groceryItems) {
+        // Consider using template literals for string concatenation
+        console.log(`* ${item}`);
     }
 }
 
-
+// Example usage:
+const groceryList = ["Apples", "Bananas", "Milk"];
+printGroceryList(groceryList);
